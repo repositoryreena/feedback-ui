12210b2ee34cb49e26e3b1120afe433b599b52f8
Reena Thomas <rtthomas@ucsc.edu>
2024-08-01 13:52:42 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+const unhappyBtn = document.getElementById("unhappy")
+const neutralBtn = document.getElementById("neutral")
+const satisfiedBtn = document.getElementById("satisfied")
+const message = document.getElementById("message")
+const sendReview = document.getElementById("sendreview")
+let selectedBtn = ""
+
+unhappyBtn.addEventListener("click", ()=>{
+    unhappyBtn.classList.add("selected")
+    neutralBtn.classList.remove("selected")
+    satisfiedBtn.classList.remove("selected")
+    selectedBtn = "Unhappy"
+})
+
+neutralBtn.addEventListener("click", ()=>{
+    neutralBtn.classList.add("selected")
+    unhappyBtn.classList.remove("selected")
+    satisfiedBtn.classList.remove("selected")
+    selectedBtn = "Neutral"
+})
+
+satisfiedBtn.addEventListener("click", ()=>{
+    satisfiedBtn.classList.add("selected")
+    neutralBtn.classList.remove("selected")
+    unhappyBtn.classList.remove("selected")
+    selectedBtn = "Satisfied"
+})
+
+sendReview.addEventListener("click", ()=>{
+    message.innerHTML = `<p><strong>Thank you! <br><br>Feedback: ${selectedBtn}</strong> <br><br>We'll use your feedback to improve<br> our customer support.</p>`
+    message.classList.add("msg")
+    })
